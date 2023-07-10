9336bfdc573e08ac5d7ee965244aa6cbe44c5aa8
Oscar <oscarmappley@icloud.com>
2023-07-10 02:32:15 +0100

    added image locations
    so that each number permanently maps onto an image, so you can get to it again. You can also see it's location as well

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # canvas of babble
  
-Contains all pixel art that has ever been made, ever will be made, all variations of all pixel art in history yadda yadda yadda. Now you can pirate every version of every pixelated show for free, you just have to be a bit lucky. The most meaningful things I've found are a 3x5 cross, an among us and a pretty cool game of life setup that lasted over 1000 steps. Use the enter key to generate a new image and the arrow keys to cycle through generated images.
+Contains all pixel art that has ever been made, ever will be made, all variations of all pixel art in history yadda yadda yadda. Now you can pirate every version of every pixelated show for free, you just have to be a bit lucky. The most meaningful things I've found are a 3x5 cross, an among us and a pretty cool game of life setup that lasted over 1000 steps.
+
+Use the enter key to generate a new image and the arrow keys to cycle through generated images. Use slash to show the image location and the more than/less than symbols to change the size of it's text. It gets very slow though when you have it on, but this library of babel is focused on simpler and smaller images so it's fine.
--- main.py
+++ main.py
@@ -1,20 +1,53 @@
 import pygame as pg
 from PIL import Image
 import numpy as np
+import random
+import sys
 
-def random_image():
+sys.setrecursionlimit(1000000000)
+sys.set_int_max_str_digits(1000000000)
+
+def base_converter(num, new_base):
+    a = num // new_base
+    b = num % new_base
+    if a >= new_base:
+        a = base_converter(a, new_base)
+    return [a, b]
+
+
+def flatten(messy_array):
+    new = messy_array.replace('[', '')
+    new = new.replace(']', '')
+    new = '[' + new + ']'
+    return eval(new)
+
+
+def index_to_image(index, colour_type):
     if colour_type == 'b&w':
-        array = np.random.randint(0, 2, size=image_width*image_height)
+        # slicing removes '0b' from the front of the string
+        index_base_2 = bin(index)[2:]
+        zero_padding = '0' * (image_pixel_count - len(index_base_2))
+        index_base_2 = zero_padding + index_base_2
+
+        array = np.array(list(index_base_2), dtype=np.uint8)
         array = np.where(array < 0.5, 0, 255)
         array = np.tile(array, (3, 1)).T
-
+    
     elif colour_type == 'grey':
-        array = np.random.randint(0, 255, size=image_width*image_height)
+        index_base_256 = flatten(str(base_converter(index, new_base=256)))
+        zero_padding = [0] * (image_pixel_count - len(index_base_256))
+        index_base_256 = zero_padding + index_base_256
+
+        array = np.array(index_base_256, dtype=np.uint8)
         array = np.tile(array, (3, 1)).T
     
     elif colour_type == 'full':
-        array = np.random.randint(0, 255, size=image_width*image_height*3)
+        index_base_256 = flatten(str(base_converter(index, new_base=256)))
+        zero_padding = [0] * (image_pixel_count * 3 - len(index_base_256))
+        index_base_256 = zero_padding + index_base_256
 
+        array = np.array(index_base_256, dtype=np.uint8)
+    
     array = np.reshape(array, (image_width, image_height, 3))
     image = pg.surfarray.make_surface(array)
 
@@ -74,15 +107,37 @@ try:
 except ValueError:
     print('Total possible images too large to compute')
 
+valid = False
+while not valid:
+    image_index = input('Enter an image location between 0 and the number above: ')
+    try:
+        if '.' in image_index:
+            raise ValueError
+        
+        image_index = int(image_index)
+
+        if image_index < 0 or image_index > total_images:
+            raise ValueError
+        
+        valid = True
+    except ValueError:
+        print('That was not a valid number. Enter a number between the range 0 and the total number of possible images')
+
 pg.init()
 screen = pg.display.set_mode((0, 0), pg.FULLSCREEN)
 screen_width, screen_height = screen.get_size()
 clock = pg.time.Clock()
+font_size = 35
+font = pg.font.SysFont('menlo', font_size)
+text_width, text_height = font.size('a')
+max_characters = screen_width // text_width
 
 held_down = False
-image = random_image()
-image_history = [image]
-image_index = 0
+image = index_to_image(image_index, colour_type)
+index_history = [image_index]
+history_index = 0
+
+viewing_index = False
 
 running = True
 while running:
@@ -95,28 +150,52 @@ while running:
             if event.key == pg.K_RETURN:
                 held_down = True
             
+            if event.key == pg.K_SLASH:
+                viewing_index = not viewing_index
+            
+            if event.key == pg.K_COMMA:
+                font_size -= 1
+                font = pg.font.SysFont('menlo', font_size)
+                text_width, text_height = font.size('a')
+                max_characters = screen_width // text_width
+            
+            if event.key == pg.K_PERIOD:
+                font_size += 1
+                font = pg.font.SysFont('menlo', font_size)
+                text_width, text_height = font.size('a')
+                max_characters = screen_width // text_width
+            
             if event.key == pg.K_LEFT:
-                image_index -= 1
-                if image_index < 0:
-                    image_index = 0
+                history_index -= 1
+                if history_index < 0:
+                    history_index = 0
             
             if event.key == pg.K_RIGHT:
-                image_index += 1
-                if image_index >= len(image_history):
-                    image_index = len(image_history) - 1
+                history_index += 1
+                if history_index >= len(index_history):
+                    history_index = len(index_history) - 1
         
         if event.type == pg.KEYUP:
             if event.key == pg.K_RETURN:
                 held_down = False
     
     if held_down:
-        image_history.append(random_image())
-        image_index = len(image_history) - 1
+        index_history.append(random.randint(0, total_images))
+        history_index = len(index_history) - 1
     
-    image = image_history[image_index]
+    image_index = index_history[history_index]
+    image = index_to_image(image_index, colour_type)
     
     image = pg.transform.scale(image, (screen_width, screen_height))
     screen.blit(image, (0, 0))
+
+    if viewing_index:
+        for colour in [[0,0,0], [255,255,255]]:
+            # add a newline character after every n characters. n is the max_characters allowed on one line
+            image_index_str = '\n'.join([f'{image_index:,}'[i:i+max_characters] for i in range(0, len(f'{image_index:,}'), max_characters)])
+            index_render = font.render(image_index_str, True, colour)
+            index_render_rect = index_render.get_rect(midtop=(screen_width/2, 12))
+            screen.blit(index_render, index_render_rect)
     
     clock.tick(5)
     pg.display.flip()
